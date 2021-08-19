a40cc75f61ffbad4cf5fe6f9f1ce38e954e3843d
Pedro A. González <pgonzalez@post.com>
2021-08-18 23:38:15 -0500

    Cambiar API_URL, cambiar plantillas JS por elementos del DOM, estilos

--- app.js
+++ app.js
@@ -7,7 +7,8 @@
   const formCrearMascota = document.getElementById('formCrearMascota');
   const contenedorMascotas = document.getElementById('contenedorMascotas');
   const spinner = document.getElementById('spinner');
-  const API_URL = 'https://crudcrud.com/api/cba653e00039415abc41a2f46051ee14/mascotas';
+  
+  const API_URL = 'https://crudcrud.com/api/fd194da2acd3418ebc4c8474ccd9bcd1/mascotas';
 
   // 2. CREAMOS FUNCIÓN PARA TRAER MASCOTAS
   const getMascotas = async () => {
@@ -23,18 +24,46 @@
   // 3. CREAMOS FUNCIÓN PARA RENDERIZAR EN HTML LAS MASCOTAS QUE VIENEN DEL API
   const renderizarMascotas = (mascotas) => {
     spinner.style.display = 'none';
-    return mascotas.map(mascota => {
+    mascotas.map(mascota => {
       const { nombre, raza, caricatura, descripcion, imagen } = mascota;
-      return `<div class="col" style="max-width:300px">
-                            <div class="card">
-                              <img src="${imagen}" class="card-img-top" alt="${imagen}">
-                              <div class="card-body">
-                                <h5 class="card-title">${nombre} <span class="badge rounded-pill bg-primary">${raza}</span></h5>
-                                <p class="card-text">${caricatura}</p>
-                                <p class="small">${descripcion}</p>
-                              </div>
-                            </div>
-                          </div>`
+
+      const divCol = document.createElement('div')
+      divCol.setAttribute('class', 'col')
+      divCol.setAttribute('style', 'max-width: 300px');
+
+      const divCard = document.createElement('div')
+      divCard.setAttribute('class', 'card');
+
+      const imgTag = document.createElement('img')
+      imgTag.setAttribute('class', 'card-img-top')
+      imgTag.setAttribute('src', imagen)
+      imgTag.setAttribute('alt', imagen);
+
+      const divCardBody = document.createElement('div')
+      divCardBody.setAttribute('class', 'card-body');
+
+      const divCardTitle = document.createElement('div')
+      divCardTitle.setAttribute('class', 'card-title')
+      divCardTitle.innerHTML = nombre + ' ';
+
+      const spanBadgeTag = document.createElement('span')
+      spanBadgeTag.setAttribute('class', 'badge rounded-pill bg-primary')
+      spanBadgeTag.innerHTML = raza;
+
+      const divCardSubtitle = document.createElement('div')
+      divCardSubtitle.setAttribute('class', 'card-text')
+      divCardSubtitle.innerHTML = caricatura;
+
+      const divCardDescription = document.createElement('div')
+      divCardDescription.setAttribute('class', 'small')
+      divCardDescription.innerHTML = descripcion;
+
+      divCol.appendChild(divCard);
+      divCardTitle.appendChild(spanBadgeTag);
+      divCard.append(imgTag, divCardBody);
+      divCardBody.append(divCardTitle, divCardSubtitle, divCardDescription);
+
+      contenedorMascotas.appendChild(divCol);
     })
   }
 
@@ -77,7 +106,7 @@
   const init = async () => {
     contenedorMascotas.innerHTML = '';
     const mascotas = await getMascotas();
-    contenedorMascotas.innerHTML = renderizarMascotas(mascotas);
+    renderizarMascotas(mascotas);
   }
 
   init();
